24a26aca76e4b730faf0ac9f04049f0ff1bd4790
b00nedawgy <brendanrboone@gmail.com>
2024-08-06 03:06:25 -0700

    untested prototype part 1

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,45 @@
 /**
  * routes.ts
+ * 
+ * Defines the routes for the app
+ * location of definitions for screen navigation and names
  */
+import {StackScreenProps} from "@react-navigation/stack";
+import {IPlayer} from "./components/state/"
+
+/**
+ * Defines what parameters gets sent to a screen route
+ * 
+ * ::App Route Plan::
+ * 
+ * HomeScreen: choose LP type 4000 or 8000
+ * 
+ * BattleScreen: two buttons with animations for LPs
+ * 
+ * CalculationScreen: edit LP counter.
+ */
+export type IStackParamList = {
+    [route_names.HOME_SCREEN]: undefined,
+    [route_names.BATTLE_SCREEN]: {starting_LP: number},
+    [route_names.CALCULATION_SCREEN]: {player: IPlayer}
+};
+
+//routes
+export interface IRoutes {
+    HOME_SCREEN: "Home",
+    BATTLE_SCREEN: "Battle",
+    CALCULATION_SCREEN: "Calculation"
+}
+
+const route_names: IRoutes = {
+    HOME_SCREEN: "Home",
+    BATTLE_SCREEN: "Battle",
+    CALCULATION_SCREEN: "Calculation"
+}
+
+// Stack Screens
+export type IHomeScreenProps = StackScreenProps<IStackParamList, typeof route_names.HOME_SCREEN>;
+export type IBattleScreenProps = StackScreenProps<IStackParamList, typeof route_names.BATTLE_SCREEN>;
+export type ICalculationScreenProps = StackScreenProps<IStackParamList, typeof route_names.CALCULATION_SCREEN>;
+
+export default route_names;
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,39 @@
+/**
+ * HomeScreen.tsx
+ * 
+ * Home Screen component.
+ */
+import {useState, useEffect} from "react";
+import {
+    StyleSheet,
+    Text,
+    View
+} from "react-native";
+import {DemoButton} from "../components/ui/DemoButton";
+import { IHomeScreenProps } from "../routes";
+
+export default function HomeScreen(props: IHomeScreenProps) {
+
+
+    return (
+        <View style={styles.container}>
+            <View style={{height: 60}}>
+                <DemoButton key="Process Image" onPress={() => console.log("HelloWorld!")}>
+                    {"DUEL!"}
+                </DemoButton>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold'
+    }
+});
